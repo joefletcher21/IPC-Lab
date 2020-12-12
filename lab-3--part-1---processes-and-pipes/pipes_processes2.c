4547520d8f4393735845aa1f5b9cc6deebe91872
Joseph Fletcher <jckmfletcher@gmail.com>
2020-12-12 14:06:27 -0500

    Update pipes_processes2.c

--- lab-3--part-1---processes-and-pipes/pipes_processes2.c
+++ lab-3--part-1---processes-and-pipes/pipes_processes2.c
@@ -2,93 +2,61 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <sys/stat.h> 
-#include <sys/wait.h> 
-#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
- * Executes the command "cat scores | grep Lakers | sort".  In this quick-and-dirty
+ * Executes the command "cat scores | grep Lakers".  In this quick-and-dirty
  * implementation the parent doesn't wait for the child to finish and
  * so the command prompt may reappear before the child terminates.
  *
- */
-/*
- * Help here:
- * https://linuxhint.com/dup2_system_call_c/
- *http://www.rozmichelle.com/pipes-forks-dups/
  */
 
 int main(int argc, char **argv)
 {
   int pipefd[2];
-	int pipefd2[2];
   int pid;
-  int pid2;
-  int waitTime;
 
   char *cat_args[] = {"cat", "scores", NULL};
-  char *grep_args[] = {"grep", argv[1], NULL};
-	char *sort_args[] = {"sort", NULL};
+  char *grep_args[] = {"grep", "Lakers", NULL};
 
   // make a pipe (fds go in pipefd[0] and pipefd[1])
 
   pipe(pipefd);
-  pipe(pipefd2);
 
   pid = fork();
 
- if (pid == 0)//works
+  if (pid == 0)
     {
+      
       // child gets here and handles "grep Villanova"
 
       // replace standard input with input part of pipe
 
-      dup2(pipefd[0], STDIN_FILENO);
-      dup2(pipefd2[1], STDOUT_FILENO);
+      dup2(pipefd[0], 0);
 
-      // close unused output half of pipe
+      // close unused hald of pipe
 
       close(pipefd[1]);
-      close(pipefd2[0]);
 
       // execute grep
 
       execvp("grep", grep_args);
-      exit(0);
     }
-  else{
-      pid = fork();
-      
-      if(pid == 0){
-        
-        dup2(pipefd2[0], STDIN_FILENO);
-        
-        close(pipefd[1]);
-        close(pipefd[0]);
-        close(pipefd2[1]);
-        
-        execvp("sort", sort_args);
-        exit(0);
-      } else{
-        
-        // parent gets here and handles "cat scores"
+  else
+    {
+      // parent gets here and handles "cat scores"
 
-        // replace standard output with output part of pipe
+      // replace standard output with output part of pipe
 
-        dup2(pipefd[1], STDOUT_FILENO);
+      dup2(pipefd[1], 1);
 
-        // close unused input half of pipe
+      // close unused unput half of pipe
 
-        close(pipefd[0]);
-        close(pipefd2[0]);
-        close(pipefd[1]);
-        close(pipefd2[1]);
+      close(pipefd[0]);
 
-        // execute cat
+      // execute cat
 
-        execvp("cat", cat_args);
-        exit(0);
-        }
+      execvp("cat", cat_args);
+      wait();
     }
-  exit(0);
 }
